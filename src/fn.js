bf743c473e3186c8919d4a34538fa18f1bb96409
Seabagel <hsrt1367tim@gmail.com>
2020-11-17 12:57:06 -0800

    Setting up for dynamic commands

---
+++ src/fn.js
@@ -0,0 +1,42 @@
+const fetch = require("node-fetch");
+
+// Functions
+const getAPI = async (handleAPI) => {
+    let response = await fetch(handleAPI.url);
+    let result = await response.json();
+    return result.then(handleAPI.func);
+};
+
+const capitalize = (str) => {
+    if (typeof str !== "string") return "";
+    return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+// Look for dictionary words that matches
+const containsWord = (wordList, message, callback) => {
+    for (let i = 0; i < wordList.length; i++) {
+        if (wordList[i].includes(message)) {
+            // Break from loop
+            callback();
+            break;
+        }
+    }
+};
+
+const mentionsKeyword = (keywords, message) => {
+    let passed = false;
+    keywords.forEach((index) => {
+        if (message.includes(index)) {
+            passed = true;
+            console.log("For loop break");
+        }
+    });
+    return passed;
+};
+
+module.exports = {
+    getAPI,
+    capitalize,
+    containsWord,
+    mentionsKeyword,
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // Dependencies
 const Discord = require("discord.js");
-const fetch = require("node-fetch");
 
-// Client/Token
+// Functions
+const { mentionsKeyword, containsWord, getAPI } = require("./fn.js");
+
+// Config files
+const { token, keywords, commonWords } = require("../config/config.json");
+
+// Client
 const client = new Discord.Client();
-const { token } = require("../config/config.json");
-
-// Variables
-const dictionary = require("../config/dictionary");
-const keywords = ["pudding", "pudd", "pud"];
 
 // Ready message
 client.once("ready", () => {
@@ -17,55 +17,16 @@ client.once("ready", () => {
 
 // Activate on message event
 client.on("message", async (message) => {
-    // variables
-    let pass = false;
-    let chat = message.content.toLowerCase(); // The chat message
+    // Message content
+    let message = message.content.toLowerCase(); // The chat message
 
-    // Check if it contains keywords
-    for (let i = 0; i < keywords.length; i++) {
-        if (chat.includes(keywords[i])) {
-            pass = true;
-            break;
-        }
-    }
+    // Check if message contains keyword
+    if (!mentionsKeyword(keywords, message) || message.author.bot) return;
 
-    // If contain keywords, continue
-    if (!pass || message.author.bot) return;
-    else {
-        // Look for dictionary words that matches
-        for (let i = 0; i < dictionary.length; i++) {
-            if (chat.includes(dictionary[i])) {
-                // Concat the resulting bible verse
-                getBibleVerse().then((result) => {
-                    const text = `${capitalize(result[0].text)}, *${
-                        result[0].bookname
-                    } - ${result[0].chapter}:${result[0].verse}*`;
-                    // Send a message
-                    message.channel.send(text);
-                });
-                // Break from loop
-                break;
-            }
-        }
-    }
+    containsWord(commonWords, message, getAPI());
+
     // EOF
 });
 
-// Functions
-async function getBibleVerse() {
-    let response = await fetch(
-        "https://labs.bible.org/api/?passage=random&type=json"
-    );
-    let result = await response.json();
-    return result;
-}
-
-const capitalize = (s) => {
-    if (typeof s !== "string") return "";
-    return s.charAt(0).toUpperCase() + s.slice(1);
-};
-
 // Last line
 client.login(token);
-
-// Test
