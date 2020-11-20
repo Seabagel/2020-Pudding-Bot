854f4c66febdb76a2f2483b87bc4633be682bd00
Seabagel <hsrt1367tim@gmail.com>
2020-11-20 14:50:44 -0800

    Updated version
    Cleaned up index to prepare for streamlining commands

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,5 @@
-// Dependencies
 const Discord = require("discord.js");
-const requestPromise = require("request-promise");
-
-// Config files
 const { token } = require("../../config/config.json");
-
-// JSON files
-const keywords = require("../json/keywords.json");
 const prefix = "pudding";
 
 // Functions
@@ -20,31 +13,23 @@ client.once("ready", () => {
 
 // Activate on message event
 client.on("message", async (userInput) => {
-    // Message content
     let message = userInput.content.toLowerCase(); // The chat message
 
-    // Check if message is from itself
     if (userInput.author.bot) return;
 
-    // If message starts with command, then
     if (message.startsWith(prefix + " -time")) {
         const args = message
             .slice((prefix + " -time").length)
             .trim()
             .split(" ");
-
-        // run command
         sendTime(userInput, args);
     } else if (message.startsWith(prefix + " -help")) {
         sendGithub(userInput);
     } else if (message.startsWith(prefix + " -preach")) {
         sendBible(userInput);
     } else {
-        // If nothing else, exit
         return;
     }
-
-    // EOF
 });
 
 // Last line
