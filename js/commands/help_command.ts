85443d2f320c1a316f695ed3e996756b13f690e5
Seabagel <hsrt1367tim@gmail.com>
2020-11-26 20:54:04 -0700

    Update help_command.ts

--- js/commands/help_command.ts
+++ js/commands/help_command.ts
@@ -13,13 +13,10 @@ import { text_tools } from "../tools/text_tool";
 // Interface
 import { command } from "../interfaces/command";
 
-let help_command: command = {
-    cmnd_name: "help",
-    async execute() {},
-};
-
-export class command_time implements command {
-    cmnd_name = "help";
+export let help_command: command = {
+    // TODO:
+    // add _ when calling the function in index.main
+    cmnd_name: "help",  
     async execute(userInput, args) {
         try {
             // Assemble embedded message
@@ -52,5 +49,44 @@ export class command_time implements command {
         } catch (error) {
             mssg_tools.catchError(userInput);
         }
-    }
-}
+    },
+};
+
+export default help_command;
+
+// export class command_time implements command {
+//     cmnd_name = "help";
+//     async execute(userInput, args) {
+//         try {
+//             // Assemble embedded message
+//             const embedded = new MessageEmbed()
+//                 .setColor(0x0099ff)
+//                 .setTitle("Commands:")
+//                 .setDescription("`pudding <command>` <arguments>")
+//                 .setThumbnail(thumbnails[2])
+//                 .addFields(
+//                     {
+//                         name: "`pudding -time` <name_of_country/city/state>",
+//                         value:
+//                             "Tells what time it is in **country/city/state**, plus a small info from wikipedia ",
+//                     },
+//                     {
+//                         name: "`pudding -help`",
+//                         value:
+//                             "What you're seeing right now, click the Command: <link> to see the github link",
+//                     },
+//                     {
+//                         name: "`pudding -preach`",
+//                         value: "Pudding reads a bible verse",
+//                     }
+//                 )
+//                 .addField(empty, mssg_tools.github(this.cmnd_name))
+//                 .setFooter(footer.text, footer.icon_url);
+
+//             // Send message
+//             mssg_tools.send(userInput, embedded, this.cmnd_name);
+//         } catch (error) {
+//             mssg_tools.catchError(userInput);
+//         }
+//     }
+// }
